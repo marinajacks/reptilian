35d06dd7ef225e52b9b2dafda389ec36777e2b57
hanyan <bchen@sparkpad.com>
2018-05-12 00:31:14 +0800

    脚本修改修改,完成全部数据修改

--- reptile.py
+++ reptile.py
@@ -44,17 +44,22 @@ def academyurl(url):
     for k in content:
         academy=[]
         academy.append(url[:url.find('/search')]+k.get('href'))
-        print(k.get('href'))  #获取对应的地址
-        print(k.string)
-        academys.append(academy)
+        #academy.append(k.string)
+        #print(k.get('href'))  #获取对应的地址
+        #print(k.string)
+        #academys.append(academy)
+        academys.append(url[:url.find('/search')]+k.get('href'))
     return academys
     
     
+academyurls = academyurl(url)
 
 
 #院系的主页url
+'''
 url='http://faculty.ecnu.edu.cn/search/teacherList.faces?siteId=10&pageId=0&nodeId=18'
-
+url='http://faculty.ecnu.edu.cn/search/teacherList.faces?siteId=10&pageId=0&nodeId=41'
+'''
 '''这个函数是根据院系的主网URL,获取到教师的个人主页url'''
 def teacherurl(url):
     res=requests.get(url)
@@ -66,15 +71,23 @@ def teacherurl(url):
     return urls
 
 
+#teacherurl(url)
+teacheradds=[]
+for url in academyurls:
+    if(len(teacherurl(url))==0):
+        urls=academyurl(url)
+        for url1 in urls:
+            teacheradds.append(teacherurl(url1))
+    else:
+        teacheradds.append(teacherurl(url))
+        
+    
 
 #这个函数是为了获取老师的信息
-url='http://faculty.ecnu.edu.cn/s/3140/main.jspy'
-
-url='http://faculty.ecnu.edu.cn/s/436/main.jspy'
 
 url='http://faculty.ecnu.edu.cn/s/920/main.jspy'
 
-url='http://faculty.ecnu.edu.cn/s/1567/main.jspy'
+#url='http://faculty.ecnu.edu.cn/s/2673/main.jspy'
 '''teacherinfo函数是为了获取老师的个人信息,输入的信息是老师的个人主页url'''
 def teacherinfo(url):
     res=requests.get(url)
@@ -86,8 +99,12 @@ def teacherinfo(url):
     content00=soup.find_all(id="touxiang") #获取头像
     touxiang=url[:url.find('/s')]+content00[0].find('img').get('src')
     '''
+    
     content01=soup.find_all(id="zhicheng") #获取职称
-    zhicheng=content01[0].string.strip()
+    if('NoneType' in str(type(content01[0].find('table')))):
+        zhicheng=''
+    else:
+        zhicheng=content01[0].string.strip()
     
     content02=soup.find_all(id="zhiwu")  #获取职务
     if('NoneType' in str(type(content02[0].find('table')))):
@@ -127,8 +144,11 @@ def teacherinfo(url):
     content6=soup.find_all(id="zywz")  #获取主页网址
     if('NoneType' in str(type(content6[0].find('table')))):
         zywz=''
-    else:
-        zywz=url[:url.find('/s')]+content6[0].find('table').find('td').find('tr').getText().strip()
+    else:#这里有一点需要注意的是,如果是给出的是www开头的地址,那么这个地址就是老师的主页网址,否则就需要字符串拼起来
+        if('www' in content6[0].find('table').find('td').find('tr').getText().strip()):
+            zywz=content6[0].find('table').find('td').find('tr').getText().strip()
+        else:
+            zywz=url[:url.find('/s')]+content6[0].find('table').find('td').find('tr').getText().strip()
         
     content7=soup.find_all(id="bgdd")  #获取办公地址
     if('NoneType' in str(type(content7[0].find('table')))):
@@ -172,23 +192,22 @@ def teacherinfo(url):
              'zhiwu':[zhiwu],'danwei':[danwei],'danwei2':[danwei2],'lxdh':[lxdh],
              'bgsj':[bgsj],'cz':[cz],'zywz':[zywz],'bgdd':[bgdd],'dzyx':[dzyx],
              'txdz':[txdz],'yjfx':[yjfx],'shjz':[shjz],'xscg':[xscg]}
+    
+    teacher1={'xingming':xingming,'zhicheng':zhicheng,'zhiwu':zhiwu,  ＃这里是用了数据字典的方式,这里是有一定的问题的.
+              'danwei':danwei,'danwei2':danwei2,'lxdh':lxdh,'bgsj':bgsj,
+              'cz':cz,'zywz':zywz,'bgdd':bgdd,'dzyx':dzyx,'txdz':txdz}
     '''
-    teacher1={'xingming':[xingming],'zhicheng':[zhicheng],'zhiwu':[zhiwu],
-              'danwei':[danwei],'danwei2':[danwei2],'lxdh':[lxdh],'bgsj':[bgsj],
-              'cz':[cz],'zywz':[zywz],'bgdd':[bgdd],'dzyx':[dzyx],'txdz':[txdz]}
-    return teacher1
+    teacher2=[xingming,zhicheng,zhiwu,danwei,danwei2,lxdh,bgsj,cz,zywz,bgdd,dzyx,txdz]
+    return teacher2
     
-    
-   
-    
-
+#测试给出教师的信息.
+a=teacherinfo(url)
 
 
 url1='http://faculty.ecnu.edu.cn/search/teacherList.faces?siteId=10&pageId=0&nodeId=59'
 url2='http://faculty.ecnu.edu.cn/search/teacherList.faces?siteId=10&pageId=0&nodeId=12'
     
 
-teacherinfo(url1)
     
 '''
 def getname(n,name):
@@ -196,27 +215,82 @@ def getname(n,name):
     print(name+'=content'+str(n)+'[0].string.strip()')
 '''    
 
-'''在将数据写入数据库之前,需要在数据库中建立相关的表,然后再将数据导入到数据库中,
+'''在将数据写入数据库之前,需要在数据库中建立相关的表,然后再将数据导入到数据库中,'''  
 
-
-
-
-'''  
-
-'''
-    
 #下面的函数是为了进行数据的写入,将数据写入到数据库中
 import pymysql
-import pandas as pd
 conn = pymysql.Connect(
      host='localhost',
      port=3306,
      user='root',
-     passwd='123456'
+     passwd='123456',
+     db='ecnu',
+     charset='utf8'
  )
 cursor = conn.cursor()
-sql="select * from test.qq"
 
-df=pd.read_sql(sql,conn)
+effect_row = cursor.executemany("insert into teacher(id,xingming,zhicheng,zhiwu,danwei,danwei2,lxdh,bgsj,cz,zywz,bgdd,dzyx,txdz) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                                ,[(1,a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9],a[10],a[11])])
+conn.commit()
+cursor.close()
+conn.close()
 
-'''
+#下面测测试如何把数据写入到数据库中,这里构造一个函数
+def writebase(a):
+    import pymysql
+    conn = pymysql.Connect(
+         host='localhost',
+         port=3306,
+         user='root',
+         passwd='123456',
+         db='ecnu',
+         charset='utf8'
+     )
+    cursor = conn.cursor()
+    
+    cursor.executemany("insert into teacher(xingming,zhicheng,zhiwu,danwei,danwei2,lxdh,bgsj,cz,zywz,bgdd,dzyx,txdz) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                                    ,[(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9],a[10],a[11])])
+    conn.commit()
+    cursor.close()
+    conn.close()
+        
+    
+    
+'''这个用来单独的处理一些数据的情况
+''' 
+url='http://faculty.ecnu.edu.cn/search/teacherList.faces?siteId=10&pageId=0&nodeId=41'
+teacherurls=teacherurl(url)
+for url in teacherurls:
+    a=teacherinfo(url)
+    print(len(a))
+    writebase(a)
+    
+    
+
+#将所有的数据都写入到数据库中
+for urls in teacheradds:
+    for url in urls:
+        a=teacherinfo(url)
+        print(len(a))
+        writebase(a)
+
+#获取每条数据
+urls1=[]
+for urls in teacheradds:
+    for url in urls:
+        urls1.append(url)
+
+#处理异常数据的方法
+def geturls(n):
+    urls2=urls1[n:len(urls1)]
+    return urls2
+
+#异常数据的处理
+urls2=geturls(0)
+for url in urls2:
+    a=teacherinfo(url)
+    print(len(a))
+    writebase(a)
+
+
+
