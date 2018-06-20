9a6768733abe13578f2e9c4ab842d696c23a7542
marinajacks <chenbiaozainan@163.com>
2018-06-20 09:56:04 +0800

    新的页面数据的提供

--- medicine/medicine2.py
+++ medicine/medicine2.py
@@ -7,43 +7,100 @@ Created on Mon Jun 18 22:55:08 2018
 这个部分主要是为了进行模拟登陆的操作,然后进行模拟控制的操作.
 """
 
-import requests as res 
+import requests 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import pandas as pd
+from selenium.webdriver.common.keys import Keys  
+import urllib.request
+import time
 
-def getdurg(drugname,p,n):
-    #url='http://www.chemcpd.csdb.cn/cmpref/Tcm_Multi/R_tcd_Comp.asp'
-    #headers = {'User-Agent': 'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
-    #response=requests.get(url,headers=headers)
-    #现在我们发现为什么在使用编码的时候出现原始的代码的问题,主要就是原始的代码出现父编码与本层的编码
-    #格式出现不一致的问题.另外,从这里可以看到,实际上我们需要使用到动态的网页爬虫技术来进行网页的登陆
-    path='/Users/macbook/downloads/geckodriver'
-    driver = webdriver.Firefox(executable_path =path)
-    url1='http://lsp.nwu.edu.cn/tcmspsearch.php?qs=herb_all_name&q=&token=725a2aa9848eaf89e7c0a2fc98a61d96'
-    driver.get(url1)
+def geturls(url):
+    #下面的数据主要是为了获取到页面的药品链接信息
+    #url='http://www.megabionet.org/tcmid/herb/5615/'
+    #url='http://www.megabionet.org/tcmid/herb/2186/'
+    headers = {'User-Agent': 'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+    response=requests.get(url,headers=headers)
+    urls=[]
+    soup=BeautifulSoup(response.text,'html.parser')
+    drugs=soup.find_all(class_='table table-striped table-bordered table-hover')[1]
+    for i in drugs.find_all('tr'):
+        j=i.find_all('td')[0]#J
+        if(j.find('a') is None):
+            print(' ')
+        else:
+            if('tcmid' in (j.find('a')['href'])):
+                #print('www.megabionet.org/'+j.find('a')['href'])
+                urls.append('http://www.megabionet.org/'+j.find('a')['href'])
+            else:
+                print(j.find('a').string)
+    
+    return urls
     
     
     
-    driver.find_element_by_class_name('input-xlarge').clear()
+
+
+
+
+#这里的操作主要是解析页面对应的文本信息
+def durginfo(url):
+    #url='http://www.megabionet.org/tcmid/ingredient/31556/'
+    headers = {'User-Agent': 'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+    response=requests.get(url,headers=headers)
+    r=response.text
+    #b = r.encode('ISO-8859-1').decode(response.apparent_encoding)
+    #path='D:\project\reptilian\medicine\imgs'
+    soup=BeautifulSoup(r,'html.parser')
+    #￥title=soup.find(class_='title text-font').string.strip()
+    titles=soup.find(class_='title text-font').string.strip().replace('Ingredient -- ','')
+    formula=soup.find_all(class_='section-text text-font')[0].string.strip()
+    pubchemid=soup.find_all(class_='section-text text-font')[2].string.strip()
+    smile=soup.find_all(class_='section-text text-font')[3].string.strip()
+    #structure=url.split('//')[1].split('/')[0]+soup.find_all(class_='section-text text-font')[4].find("img").get('src')
+    #imgs=title.split('--')[1].strip()+"."+structure.split('/')[-1].split('.')[1]
+    #paths=path+'\\'+imgs
     
-    driver.find_element_by_id('inputVarTcm').send_keys('浙贝母')
-    
-    driver.find_element_by_id('searchBtTcm').click()
-    
-    
-    #下面是模拟登陆的页面
-    driver.find_element_by_name('Username').clear()   #清除用户名字
-    driver.find_element_by_name('Username').send_keys('marina')#输入用户名
-    driver.find_element_by_name('Password').clear()   #清除用户密码
-    driver.find_element_by_name('Password').send_keys('han#1990@yan')#输入用户密码
-    driver.find_element_by_name('login').click()   #点击登陆
-    #文本检索数据信息
-    driver.find_element_by_link_text('中药药材检索').click()
-    #driver.find_element_by_link_text('中药药材检索').get_attribute("href") 这部分的数据主要是为了
-    #获取到中药药材检索对应的href数据,其实不一定是为了获取到这些数据,还可以直接click来跳转到对应的网页
-    
-    driver.find_element_by_name('Specname').clear()
-    driver.find_element_by_name('Specname').send_keys(drugname)
-    driver.find_element_by_id('submit1').click()
-    driver.find_element_by_name('FID').click()
+    info=[]
+    info.append(titles)
+    info.append(formula)
+    info.append(pubchemid)
+    info.append(smile)
+    return info
+
+
+def getdrug(drugname):
+    #这个是通过模拟人的行为找到对应的药物的网页
+    path='D:\project\selenium\geckodriver'
+    driver = webdriver.Firefox(executable_path=path)
+    url='http://www.megabionet.org/tcmid/'
+    driver.get(url)
+    #模拟查询药物的相关操作
+    driver.find_element_by_link_text('Search').click()
+    driver.find_element_by_id('Channel12').click()
+    driver.find_element_by_id('id_chinese_Name').clear()
+    driver.find_element_by_id('id_chinese_Name').send_keys(drugname)
+    driver.current_window_handle #页面会发生跳转,这个命令用来将driver页面转换
+    time.sleep(1)
+    driver.find_element_by_id('id_chinese_Name').send_keys(Keys.ENTER)
+    time.sleep(1)
+    driver.switch_to_window(driver.window_handles[1])
+    url1=driver.current_url
+    return url1
+
+
+if __name__=='__main__':
+    drugname=input('请输入中药名称:')
+    url=getdrug(drugname)
+    num=input('输入页面个数:')
+    p=r'D:\project\reptilian\medicine'
+    urls=geturls(url)
+    drugs=[]
+    drugs.append(['titles','formula','pubchemid','smile'])
+    for url0 in urls:
+        print(durginfo(url0))
+        drugs.append(durginfo(url0))
+    drug=pd.DataFrame(drugs)
+    p=p+'\\'+drugname+num+'.xlsx'
+    drug.to_excel(p,sheet_name=drugname,header =False)
+        
