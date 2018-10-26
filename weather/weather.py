e9ff8815b9ee64001545f898a785b4ed97c28fa2
marinajacks <chenbiaozainan@163.com>
2018-10-26 16:33:13 +0800

    天气数据获取

---
+++ weather/weather.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 19 14:25:43 2018
+
+@author: macbook
+本项目主要是为了获取到全国的天气数据,并将这些数据存储到数据库
+或者是txt中,考虑到存储在txt中实际上后续还需要做相关的调整,暂
+时的处理思路就是将数据存储在数据库中
+"""
+
+import requests
+
+from bs4 import BeautifulSoup
+
+
+
+
+
+
+
+
+
+
+if __name__== "__main__":
+    print("hello world")
